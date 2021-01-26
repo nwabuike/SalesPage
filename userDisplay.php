707f78521474ad7db6fef6d95a969496aeb0d785
nwabuike <nwabuikeanthony@gmail.com>
2021-01-26 22:35:22 +0100

    user display added

---
+++ userDisplay.php
@@ -0,0 +1,93 @@
+<?php
+include_once './php/db_connection.php';
+$result = mysqli_query($conn,"SELECT * FROM users");
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"
+    />
+    <!--Import materialize.css-->
+    <link
+      type="text/css"
+      rel="stylesheet"
+      href="css/materialize.min.css"
+      media="screen,projection"
+    />
+    <link
+      type="text/css"
+      rel="stylesheet"
+      href="css/style.css"
+      media="screen,projection"
+    />
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Prep50 | Books</title>
+
+    <!-- <!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="js/jquery-2.2.2.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+  </head>
+  <body>
+    <nav style="margin-bottom: 20px; margin-top: 10px">
+      <div class="nav-wrapper">
+        <h3 class="center">Prep50 Books</h3>
+      </div>
+    </nav>
+    <?php
+if (mysqli_num_rows($result) >
+    0) { ?>
+    <div class="container" style="margin-bottom: 50px">
+      <div class="">
+        <table>
+          <thead>
+            <tr>
+              <th>S/N</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone Number</th>
+              <th>Address</th>
+              <th>Waec Bundle</th>
+              <th>Jamb Bundle</th>
+            </tr>
+          </thead>
+          <?php
+$i=0;
+while($row = mysqli_fetch_array($result)) {
+?>
+
+          <tbody>
+            <tr>
+              <td><?php echo $row["Id"]; ?></td>
+              <td><?php echo $row["fullname"]; ?></td>
+              <td><?php echo $row["email"]; ?>r</td>
+              <td><?php echo $row["phone"]; ?></td>
+              <td><?php echo $row["address"]; ?></td>
+              <td><?php echo $row["waecBundle"]; ?></td>
+              <td><?php echo $row["jambBundle"]; ?></td>
+            </tr>
+          </tbody>
+          <?php
+$i++;
+}
+?>
+        </table>
+        <?php
+            }
+            else{
+                echo "No result found";
+            }
+            ?>
+      </div>
+    </div>
+    <footer class="page-footer">
+      <div class="footer-copyright">
+        <div class="container">© 2021 Copyright Prep50Books</div>
+      </div>
+    </footer>
+  </body>
+</html>
